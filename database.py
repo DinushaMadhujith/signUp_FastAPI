ebf4d3016c9e8a178afd72203612cabeeac68177
Dinusha Madhujith <shdinu888@gmail.com>
2025-01-25 15:58:52 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = "postgresql://greenhaven_user:kokf1ht3QVLf3UNihKqISjwzjki00I2I@dpg-ct7ked3qf0us73dp8140-a.oregon-postgres.render.com/greenhaven"
+URL_DATABASE = "postgresql://greenhaven_wvym_user:DBMRGKCTHctAsqECEwyxi2t6O5VGLZgZ@dpg-cuabmh1u0jms73fn6pp0-a.oregon-postgres.render.com/greenhaven_wvym"
+
 
 engine = create_engine(URL_DATABASE)
 
